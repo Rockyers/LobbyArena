458e993f3f33f1d06de54460bdc8f04aa2ace4fd
Rockyers <ethan@yocom.org>
2023-07-31 23:55:50 -0700

    Initial Plugin

---
+++ src/main/java/me/rockyers/lobbyarena/LobbyArena.java
@@ -0,0 +1,17 @@
+package me.rockyers.lobbyarena;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LobbyArena extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
